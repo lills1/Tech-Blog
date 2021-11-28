781c406ccf5770a43338b07a9cfa3740715590a7
lills1 <lilly.stephenson4@gmail.com>
2021-11-28 11:57:24 +1100

    modified to add comments

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const postRoutes = require('./postRoutes');
+const commentRoutes = require('./commentRoutes');
 
+router.use('/comment', commentRoutes);
 router.use('/users', userRoutes);
 router.use('/post', postRoutes);
 
