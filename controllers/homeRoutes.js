7c706cb875f7c799ae3847a01e3b5655110e656c
lills1 <lilly.stephenson4@gmail.com>
2021-11-29 12:26:52 +1100

    modified homeroutes to get everything working properly

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,30 +1,32 @@
 const router = require('express').Router();
-const { Post, User } = require('../models');
+const { Post, User, Comment } = require('../models');
 const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
-  try {
-    // Get all projects and JOIN with user data
-    const postData = await Post.findAll({
-      include: [
-        {
-          model: User,
-          attributes: ['username'],
-        },
-      ],
-    });
+  // try {
+  // Get all projects and JOIN with user data
+  const postData = await Post.findAll({
+    include: [
+      {
+        model: User,
+      },
+      {
+        model: Comment
+      }
+    ],
+  });
 
-    // Serialize data so the template can read it
-    const posts = postData.map((post) => post.get({ plain: true }));
-
-    // Pass serialized data and session flag into template
-    res.render('homepage', {
-      posts,
-      logged_in: req.session.logged_in
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
+  // Serialize data so the template can read it
+  const posts = postData.map((post) => post.get({ plain: true }));
+  console.log(posts)
+  // Pass serialized data and session flag into template
+  res.render('homepage', {
+    posts,
+    logged_in: req.session.logged_in
+  });
+  // } catch (err) {
+  //   res.status(500).json(err);
+  // }
 });
 
 router.get('/post/:id', async (req, res) => {
@@ -39,9 +41,9 @@ router.get('/post/:id', async (req, res) => {
     });
 
     const post = postData.get({ plain: true });
-
-    res.render('post', {
-      ...post,
+    console.log(post)
+    res.render('editPost', {
+      post,
       logged_in: req.session.logged_in
     });
   } catch (err) {
