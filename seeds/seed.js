dee113cac46aabfa397e55135bd26c46dea0b71d
lills1 <lilly.stephenson4@gmail.com>
2021-11-29 12:27:55 +1100

    modified to add comments

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,23 +1,24 @@
 const sequelize = require('../config/connection');
-const { User, Post } = require('../models');
+const { User, Post, Comment } = require('../models');
 
 const userData = require('./userData.json');
 const postData = require('./postData.json');
+const CommentData = require('./CommentData.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
-
   const users = await User.bulkCreate(userData, {
     individualHooks: true,
     returning: true,
   });
-
-  for (const post of postData) {
-    await Post.create({
-      ...post,
-      user_id: users[Math.floor(Math.random() * users.length)].id,
-    });
-  }
+  const posts = await Post.bulkCreate(postData, {
+    individualHooks: true,
+    returning: true,
+  });
+  constcomments = await Comment.bulkCreate(CommentData, {
+    individualHooks: true,
+    returning: true,
+  });
 
   process.exit(0);
 };
