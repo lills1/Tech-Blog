f1aaec37ade3a55207d02dc7cb47325d55961779
lills1 <lilly.stephenson4@gmail.com>
2021-11-29 12:29:18 +1100

    more changes to suit additonal functionality

--- models/index.js
+++ models/index.js
@@ -7,9 +7,24 @@ User.hasMany(Post, {
   onDelete: 'CASCADE'
 });
 
+User.hasMany(Comment, {
+  foreignKey: 'post_id',
+  onDelete: 'CASCADE'
+});
+
+Post.hasMany(Comment, {
+  foreignKey: 'post_id'
+});
+
 Post.belongsTo(User, {
   foreignKey: 'user_id'
 });
 
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id'
+});
+Comment.belongsTo(User, {
+  foreignKey: "user_id",
+});
 
 module.exports = { User, Post, Comment };
--- public/js/profile.js
+++ public/js/profile.js
@@ -21,47 +21,12 @@ const newFormHandler = async (event) => {
   }
 };
 
-const delButtonHandler = async (event) => {
-  if (event.target.hasAttribute('data-id')) {
-    const id = event.target.getAttribute('data-id');
 
-    const response = await fetch(`/api/post/${id}`, {
-      method: 'DELETE',
-    });
-
-    if (response.ok) {
-      document.location.replace('/profile');
-    } else {
-      alert('Failed to delete post');
-    }
-  }
-};
-
-const UpdateButtonHandler = async (event) => {
-  alert.console("running updateNuttonJa:")
-  if (event.target.hasAttribute('data-id')) {
-    const id = event.target.getAttribute('data-id');
-
-    const response = await fetch(`/api/post/${id}`, {
-      method: 'PUT',
-    });
-
-    if (response.ok) {
-      document.location.replace('/profile');
-    } else {
-      alert('Failed to update post');
-    }
-  }
-};
 
 document
   .querySelector('.new-blog-form')
   .addEventListener('submit', newFormHandler);
 
-document
-  .querySelector('.post-list')
-  .addEventListener('click', delButtonHandler);
 
-document
-  .querySelector('.update')
-  .addEventListener('click', UpdateButtonHandler);
+
+
